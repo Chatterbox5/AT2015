c7b7badf8f867a3ea9a6f51dbcd40e0e3ca94ba8
16RC0085 <16RC0085@HSS-101-20.westwindsor.k12.nj.us>
2015-12-08 12:37:45 -0500

    hkfg

--- AT stuff/StacksBoi/bodmas/Operands.java
+++ AT stuff/StacksBoi/bodmas/Operands.java
@@ -6,15 +6,29 @@ public class Operands {
 		
 	}
 	
-	public static int operate(String str)
+	public int operate(String str)
 	{
 		String queue = "";
+		String[] arr=str.split(" ");
 		Stack nums = new Stack<String>();
 		Stack ops = new Stack<String>();
+		int sum;
 		
-		for(int x = 0; x < str.length(); x++)
+		for(int x = 0; x < arr.length; x++)
 		{
-			if(str.indexOf(x).equals(" "));
+			if(x%2 != 0)//number
+				nums.push(arr[x]);
+			else
+			{
+				if(ops.empty() || precedence((String)ops.peek(), arr[x]))
+					
+			}
 		}
+	}
+	
+	public boolean precedence(String stackTop, String next)
+	{
+		int top, n;
+		if(stackTop)
 	}
 }
